2e21279d9713459f4980ca68b03b132e669fb287
Daniel <danielwinther@hotmail.dk>
2015-04-24 12:25:19 +0200

    update

--- 1aarsproeve/1aarsproeve/database.sql
+++ 1aarsproeve/1aarsproeve/database.sql
@@ -3,11 +3,24 @@
     [Stilling]   VARCHAR (50) NOT NULL,
     PRIMARY KEY CLUSTERED ([StillingId] ASC)
 );
+INSERT INTO Stillinger VALUES ('Butikschef');
+INSERT INTO Stillinger VALUES ('Ungarbejder');
+INSERT INTO Stillinger VALUES ('Nøglebærer');
+INSERT INTO Stillinger VALUES ('Flaskedreng');
+
 CREATE TABLE [dbo].[Ugedage] (
     [UgedagId] INT         IDENTITY (1, 1) NOT NULL,
     [Ugedag]   VARCHAR (7) NOT NULL,
     PRIMARY KEY CLUSTERED ([UgedagId] ASC)
 );
+INSERT INTO Ugedage VALUES ('Mandag');
+INSERT INTO Ugedage VALUES ('Tirsdag');
+INSERT INTO Ugedage VALUES ('Onsdag');
+INSERT INTO Ugedage VALUES ('Torsdag');
+INSERT INTO Ugedage VALUES ('Fredag');
+INSERT INTO Ugedage VALUES ('Lørdag');
+INSERT INTO Ugedage VALUES ('Søndag');
+
 CREATE TABLE [dbo].[Ansatte] (
     [Brugernavn] VARCHAR (50) NOT NULL,
     [Navn]       VARCHAR (50) NULL,
@@ -20,6 +33,8 @@ CREATE TABLE [dbo].[Ansatte] (
     PRIMARY KEY CLUSTERED ([Brugernavn] ASC),
     CONSTRAINT [FK_Ansatte_Roller] FOREIGN KEY ([StillingId]) REFERENCES [dbo].[Stillinger] ([StillingId])
 );
+INSERT INTO Ugedage VALUES ('Daniel', 'Daniel Winther', 'daniel', 'daniel@hotmail.dk', '88888888', 'Degnestavnen 99', '2400', 1);
+
 CREATE TABLE [dbo].[Vagter] (
     [VagtId]         INT          IDENTITY (1, 1) NOT NULL,
     [Starttidspunkt] TIME (0)     NOT NULL,
@@ -31,6 +46,8 @@ CREATE TABLE [dbo].[Vagter] (
     CONSTRAINT [FK_Vagter_Ugedage] FOREIGN KEY ([UgedagId]) REFERENCES [dbo].[Ugedage] ([UgedagId]),
     CONSTRAINT [FK_Vagter_Ansatte] FOREIGN KEY ([Brugernavn]) REFERENCES [dbo].[Ansatte] ([Brugernavn])
 );
+INSERT INTO Vagter VALUES ('10:00', '16:00', 18, 1, 'Daniel');
+
 CREATE TABLE [dbo].[Beskeder] (
     [BeskedId]    INT          IDENTITY (1, 1) NOT NULL,
     [Overskrift]  VARCHAR (50) NULL,
@@ -41,3 +58,4 @@ CREATE TABLE [dbo].[Beskeder] (
     PRIMARY KEY CLUSTERED ([BeskedId] ASC),
     CONSTRAINT [FK_Beskeder_Ansatte] FOREIGN KEY ([Brugernavn]) REFERENCES [dbo].[Ansatte] ([Brugernavn])
 );
+INSERT INTO Beskeder VALUES ('MUS-samtaler', '2015-04-04', 'Så er der MUS-samtaler!', '2015-05-15', 'Daniel');
