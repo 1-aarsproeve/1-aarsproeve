1416e06666e8d5a5ba817f4ef2cbe5765a64b4b6
Daniel <danielwinther@hotmail.dk>
2015-04-30 08:39:59 +0200

    database

--- 1aarsproeve/1aarsproeve/database.sql
+++ 1aarsproeve/1aarsproeve/database.sql
@@ -1,18 +1,22 @@
-﻿CREATE TABLE [dbo].[Stillinger] (
+﻿/* Opretter Stillinger-tabel */
+CREATE TABLE [dbo].[Stillinger] (
     [StillingId] INT          IDENTITY (1, 1) NOT NULL,
     [Stilling]   VARCHAR (50) NOT NULL,
     PRIMARY KEY CLUSTERED ([StillingId] ASC)
 );
+/* Indsætter i Stillinger-tabel */
 INSERT INTO Stillinger VALUES ('Butikschef');
 INSERT INTO Stillinger VALUES ('Ungarbejder');
 INSERT INTO Stillinger VALUES ('Nøglebærer');
 INSERT INTO Stillinger VALUES ('Flaskedreng');
 
+/* Opretter Ugedage-tabel */
 CREATE TABLE [dbo].[Ugedage] (
     [UgedagId] INT         IDENTITY (1, 1) NOT NULL,
     [Ugedag]   VARCHAR (7) NOT NULL,
     PRIMARY KEY CLUSTERED ([UgedagId] ASC)
 );
+/* Indsætter i Ugedage-tabel */
 INSERT INTO Ugedage VALUES ('Mandag');
 INSERT INTO Ugedage VALUES ('Tirsdag');
 INSERT INTO Ugedage VALUES ('Onsdag');
@@ -21,6 +25,7 @@ INSERT INTO Ugedage VALUES ('Fredag');
 INSERT INTO Ugedage VALUES ('Lørdag');
 INSERT INTO Ugedage VALUES ('Søndag');
 
+/* Opretter Ansatte-tabel */
 CREATE TABLE [dbo].[Ansatte] (
     [Brugernavn] VARCHAR (50) NOT NULL,
     [Navn]       VARCHAR (50) NULL,
@@ -33,8 +38,10 @@ CREATE TABLE [dbo].[Ansatte] (
     PRIMARY KEY CLUSTERED ([Brugernavn] ASC),
     CONSTRAINT [FK_Ansatte_Roller] FOREIGN KEY ([StillingId]) REFERENCES [dbo].[Stillinger] ([StillingId])
 );
-INSERT INTO Ugedage VALUES ('Daniel', 'Daniel Winther', 'daniel', 'daniel@hotmail.dk', '88888888', 'Degnestavnen 99', '2400', 1);
+/* Indsætter i Ansatte-tabel */
+INSERT INTO Ansatte VALUES ('Daniel', 'Daniel Winther', 'daniel', 'daniel@hotmail.dk', '88888888', 'Degnestavnen 99', '2400', 1);
 
+/* Opretter Vagter-tabel */
 CREATE TABLE [dbo].[Vagter] (
     [VagtId]         INT          IDENTITY (1, 1) NOT NULL,
     [Starttidspunkt] TIME (0)     NOT NULL,
@@ -46,8 +53,10 @@ CREATE TABLE [dbo].[Vagter] (
     CONSTRAINT [FK_Vagter_Ugedage] FOREIGN KEY ([UgedagId]) REFERENCES [dbo].[Ugedage] ([UgedagId]),
     CONSTRAINT [FK_Vagter_Ansatte] FOREIGN KEY ([Brugernavn]) REFERENCES [dbo].[Ansatte] ([Brugernavn])
 );
+/* Indsætter i Vagter-tabel */
 INSERT INTO Vagter VALUES ('10:00', '16:00', 18, 1, 'Daniel');
 
+/* Opretter Beskeder-tabel */
 CREATE TABLE [dbo].[Beskeder] (
     [BeskedId]    INT          IDENTITY (1, 1) NOT NULL,
     [Overskrift]  VARCHAR (50) NULL,
@@ -58,4 +67,20 @@ CREATE TABLE [dbo].[Beskeder] (
     PRIMARY KEY CLUSTERED ([BeskedId] ASC),
     CONSTRAINT [FK_Beskeder_Ansatte] FOREIGN KEY ([Brugernavn]) REFERENCES [dbo].[Ansatte] ([Brugernavn])
 );
+/* Indsætter i Besked-tabel */
 INSERT INTO Beskeder VALUES ('MUS-samtaler', '2015-04-04', 'Så er der MUS-samtaler!', '2015-05-15', 'Daniel');
+
+/* Opretter AnsatteView */
+GO CREATE VIEW [dbo].[AnsatteView]
+	AS SELECT * FROM [Ansatte]
+
+/* Opretter HovedmenuView */
+GO CREATE VIEW [dbo].[HovedmenuView]
+	AS  SELECT Beskeder.*, Ansatte.Navn FROM [Beskeder] 
+	JOIN Ansatte ON Beskeder.Brugernavn = Ansatte.Brugernavn
+	WHERE Beskeder.Udloebsdato > GETDATE()
+
+/* Opretter VagtplanView */
+ GO CREATE VIEW [dbo].[VagtplanView]
+	AS SELECT Vagter.Starttidspunkt, Vagter.Sluttidspunkt, Vagter.UgedagId, Vagter.Ugenummer, Vagter.VagtId, Ansatte.Brugernavn, Ansatte.Navn FROM [Vagter]
+	JOIN Ansatte ON Vagter.Brugernavn = Ansatte.Brugernavn
