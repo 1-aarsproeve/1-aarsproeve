88ba22ab75a6165e32fd61ffff7b48d94d4f527f
Daniel <danielwinther@hotmail.dk>
2015-05-13 08:33:29 +0200

    splash

--- 1aarsproeve/1aarsproeve/App.xaml.cs
+++ 1aarsproeve/1aarsproeve/App.xaml.cs
@@ -32,7 +32,6 @@ namespace _1aarsproeve
         public App()
         {
             this.InitializeComponent();
-            this.Suspending += OnSuspending;
         }
 
         /// <summary>
@@ -63,13 +62,13 @@ namespace _1aarsproeve
 
                 rootFrame.NavigationFailed += OnNavigationFailed;
 
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                //  Display an extended splash screen if app was not previously running.
+                if (e.PreviousExecutionState != ApplicationExecutionState.Running)
                 {
-                    //TODO: Load state from previously suspended application
+                    Splash extendedSplash = new Splash(e.SplashScreen);
+                    rootFrame.Content = extendedSplash;
+                    Window.Current.Content = rootFrame;
                 }
-
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
             }
 
             if (rootFrame.Content == null)
@@ -91,20 +90,6 @@ namespace _1aarsproeve
         void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
         {
             throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
-        }
-
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
-        /// </summary>
-        /// <param name="sender">The source of the suspend request.</param>
-        /// <param name="e">Details about the suspend request.</param>
-        private void OnSuspending(object sender, SuspendingEventArgs e)
-        {
-            var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Save application state and stop any background activity
-            deferral.Complete();
         }
     }
 }
